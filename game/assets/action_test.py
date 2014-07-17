6c4964b7b158d08c2d2c4a9a1957997357074b2c
Stephen Cattaneo <stephen.cattaneo@gmail.com>
2014-07-16 22:45:16 -0700

    minor test changes.

--- game/assets/action_test.py
+++ game/assets/action_test.py
@@ -7,7 +7,8 @@ import unittest as u
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from dungeon_master import DungeonMaster
 
-from action import *
+import action
+import room
 
 class BaseTestCase(u.TestCase):
   def setUp(self):
@@ -17,14 +18,14 @@ class BaseTestCase(u.TestCase):
 
 class GoToAction(BaseTestCase):
   def test_existing_room(self):
-    action = GoTo(dm=self.dm)
-    action.do('goto lobby'.split())
-    self.assertEqual(self.dm.get_current_room().__class__.__name__, 'Lobby')
+    goto = action.GoTo(dm=self.dm)
+    goto.do('goto lobby'.split())
+    self.assertEqual(self.dm.current_room().__class__.__name__, 'Lobby')
 
   def test_bad_room(self):
-    action = GoTo(dm=self.dm)
+    goto = action.GoTo(dm=self.dm)
     self.dm.all_rooms = {'kittens': None}
-    action.do('goto asdf'.split())
+    goto.do('goto asdf'.split())
     expected = re.compile('''Unknown room: 'asdf'\.\s+Possible rooms:\s+\* kittens''')
     self.assertTrue(expected.search(self.ostream.getvalue()))
 
